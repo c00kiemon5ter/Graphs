22e56e99cb3a3f19d5e3a41b2887a7b0ee0a6072
JesusSuperstar <mrJesusSuperstar@gmail.com>
2010-03-10 22:29:04 +0200

    netbeans default code file

---
+++ graphs/Main.java
@@ -0,0 +1,16 @@
+
+package graphs;
+
+/**
+ *
+ * @author Ivan Kanakarakis
+ */
+public class Main {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+    }
+
+}
