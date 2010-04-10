27edbc1e19ea39723feb018db4c8b2d88d34e7f9
c00kiemon5ter <ivan.kanak@gmail.com>
2010-04-11 02:48:15 +0300

    removed testing main() of OrganicDialog

--- Forms/OrganicOptionsDialog.java
+++ Forms/OrganicOptionsDialog.java
@@ -275,26 +275,6 @@ public class OrganicOptionsDialog extends javax.swing.JDialog {
 		return (Double) edgeDistSpinner.getValue();
 	}
 
-	/**
-	 * @param args the command line arguments
-	 */
-	public static void main(String args[]) {
-		java.awt.EventQueue.invokeLater(new Runnable() {
-			public void run() {
-				OrganicOptionsDialog dialog = new OrganicOptionsDialog(new javax.swing.JFrame(), true);
-				dialog.addWindowListener(new java.awt.event.WindowAdapter() {
-					@Override
-					public void windowClosing(java.awt.event.WindowEvent e) {
-						System.exit(0);
-					}
-
-				});
-				dialog.setVisible(true);
-			}
-
-		});
-	}
-
         // Variables declaration - do not modify//GEN-BEGIN:variables
         private javax.swing.JCheckBox borderLine;
         private javax.swing.JLabel borderLineLbl;
